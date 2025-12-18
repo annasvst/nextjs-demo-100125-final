a15ff9f93938405fb9b0b5ec8a1eb71412b1f78e
annasvst <annaatalay@hotmail.com>
2025-12-18 18:40:33 +0100

    Add type definition for Sometype in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 
+type Sometype = {
+  oneValue: string;
+  anotherValue: any;
+}
+
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
