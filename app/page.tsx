b2c629df13c694213a5878fed6540e92f68ae100
annasvst <annaatalay@hotmail.com>
2025-12-18 19:41:16 +0100

    Remove unused type definition for Sometype in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Image from 'next/image';
 
-type Sometype = {
-  oneValue: string;
-  anotherValue: any;
-}
-
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
