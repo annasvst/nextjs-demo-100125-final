eeb7f93c9242c6a40055626ea53a5be61af62899
annasvst <annaatalay@hotmail.com>
2025-12-18 19:17:18 +0100

    Add debug log to the Ping GET route for improved tracing

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.debug('Debug in the Ping GET route');
   console.log('Log in the Ping GET route');
   console.warn('Warn in the Ping GET route');
   console.error('Error in the Ping GET route');
